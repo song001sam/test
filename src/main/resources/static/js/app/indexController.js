02daa371e6f824f3d24e454d60e9aefb5c0163bc
song001sam <song001sam@126.com>
2019-01-26 23:24:31 +0800

    三级左树

--- src/main/resources/static/js/app/indexController.js
+++ src/main/resources/static/js/app/indexController.js
@@ -2,6 +2,9 @@
 define(function (require) {
     var app = require('./app');
     app.controller('indexController', function ($scope, $http, $state) {
+        $scope.status = {
+            open: []
+        };
         $http({
             method: "POST",
             url: "public/getUser"
@@ -19,15 +22,27 @@ define(function (require) {
         };
         $scope.groups = [
             {
-                list: [{content: "模型管理", url: "index.MXGL"}, {content: "生产数据统计分析", url: "index.TJFX"}, {
-                    content: "新钢种成分分析",
-                    url: "index.CFFX"
-                }, {
-                    content: "单工序分析",
-                    url: "index.XSMX"
-                }], title: "智能分析"
+                contentList: [
+                    {
+                        content: "模型管理", url: "index.MXGL", contentList: [
+                            {content: "模型管理", url: "index.MXGL"},
+                            {content: "模型管理", url: "index.MXGL"}
+                        ]
+                    },
+                    {
+                        content: "生产数据统计分析", url: "index.TJFX"
+                    },
+                    {
+                        content: "新钢种成分分析",
+                        url: "index.CFFX"
+                    },
+                    {
+                        content: "单工序分析",
+                        url: "index.XSMX"
+                    }
+                ], content: "智能分析"
             },
-            {list: [{content: "用户管理", url: "index.a"}, {content: "系统设置", url: "b({id:'aa'})"}], title: "系统管理"},
+            {contentList: [{content: "用户管理", url: "index.a"}, {content: "系统设置", url: "b({id:'aa'})"}], content: "系统管理"},
         ]
 
     })
